44f759ca0be6d57196d03b352289aaffc7b211de
Maria <mariaj.gc95@gmail.com>
2025-06-01 23:18:06 -0300

    token is not used anymore

--- cypress/e2e/2-advanced-examples/Firsttest.cy.js
+++ cypress/e2e/2-advanced-examples/Firsttest.cy.js
@@ -3,32 +3,31 @@
 //@ts-nocheck
 
 //const navbarText = Cypress.env('navbarText');
-const token = 'abc123'
 context('My First test', () => {
     beforeEach(() => {
         cy.visit('/commands/Actions')
     })
 
-    // it('triggers a popover on click', () =>{
-    //     cy.get('.action-btn').click()
-    //     cy.findByText('This popover shows up on click').should('be.visible')
-    // })
+    it('triggers a popover on click', () =>{
+        cy.get('.action-btn').click()
+        cy.findByText('This popover shows up on click').should('be.visible')
+    })
 
-    // it('can click on different sections of a canvas', () =>{
-    //     cy.get('#action-canvas').click('top')
-    //     cy.get('#action-canvas').click('bottomRight')
-    //     cy.get('#action-canvas').click(80, 100)
-    // })
+    it('can click on different sections of a canvas', () =>{
+        cy.get('#action-canvas').click('top')
+        cy.get('#action-canvas').click('bottomRight')
+        cy.get('#action-canvas').click(80, 100)
+    })
 
-    // it('can double click to edit', () => {
-    //     cy.get('.action-div').dblclick().should('not.be.visible')
-    //     cy.get('.action-input-hidden').should('be.visible')
-    // })
+    it('can double click to edit', () => {
+        cy.get('.action-div').dblclick().should('not.be.visible')
+        cy.get('.action-input-hidden').should('be.visible')
+    })
 
-    // it('right click to edit', () =>{
-    //     cy.get('.rightclick-action-div').rightclick().should('not.be.visible')
-    //     cy.get('.rightclick-action-input-hidden').should('be.visible')
-    // })
+    it('right click to edit', () =>{
+        cy.get('.rightclick-action-div').rightclick().should('not.be.visible')
+        cy.get('.rightclick-action-input-hidden').should('be.visible')
+    })
 
     it('shows the nav links on hover', () => {
         cy.get('.dropdown-toggle').trigger('mouseover')
